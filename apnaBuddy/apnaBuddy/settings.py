ff1af37161f8ea8fd0aaf2bed945c666bfc20a0b
Mohsin Chougale <mohsin.chougale@gmail.com>
2020-12-06 12:38:11 +0530

    static file update 1

--- apnaBuddy/apnaBuddy/settings.py
+++ apnaBuddy/apnaBuddy/settings.py
@@ -13,9 +13,11 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 
+STATIC_URL = '/static/'
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+STATICFILES_DIRS = [BASE_DIR/"static",]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
